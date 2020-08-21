9402245fe8a54422292690fcd724d91ef7dc6547
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-08-21 13:18:43 -0400

    Updates

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: black;
+  text-decoration: none;
+`;
+
+const LinkContainer = styled.div`
+  width: 150px;
+  display: flex;
+  justify-content: space-around;
+`;
+const LogoTitle = styled.h1`
+  font-size: 25px;
+  color: Red;
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`;
+
+export default function Navbar() {
+  return (
+    <NavbarContainer>
+      <LogoTitle>🐱Pizza Cat🐱</LogoTitle>
+      <LinkContainer>
+        <StyledLink to="/home">Home</StyledLink>
+        <StyledLink to="/pizza">Pizza</StyledLink>
+      </LinkContainer>
+    </NavbarContainer>
+  );
+}
